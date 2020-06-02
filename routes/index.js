37c14259d322e3aa9e870e7f78711baf42738d56
Adolfo <adolfo1997a@gmail.com>
2020-06-02 14:04:44 -0400

    Updated routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express();
+const ApiController = require('./../controllers');
 let router = express.Router();
 
 router.use(async (req, res, next) => {
@@ -7,28 +7,38 @@ router.use(async (req, res, next) => {
     next();
 });
 
+router.get('/', async (req, res) => {
+    res.send('Hello World by API');
+});
+
 router.post('/login', async (req, res) => {
-    res.send('HELLO');
+    let response = await ApiController.login(req);
+    res.send(response);
 });
 
 router.post('/register', async (req, res) => {
-    res.send('HELLO');
+    let response = await ApiController.register(req);
+    res.send(response);
 });
 
 router.get('/products', async (req, res) => {
-    res.send('HELLO');
+    let response = await ApiController.getProducts(req);
+    res.send(response);
 })
 
 router.get('/orders', async (req, res) => {
-    res.send('HELLO');
+    let response = await ApiController.getOrders(req);
+    res.send(response);
 });
 
 router.post('/orders', async (req, res) => {
-    res.send('HELLO');
+    let response = await ApiController.addOrders(req);
+    res.send(response);
 });
 
 router.post('/deliveries', async (req, res) => {
-    res.send('HELLO');
+    let response = await ApiController.addDeliveries(req);
+    res.send(response);
 });
 
 module.exports = router;
