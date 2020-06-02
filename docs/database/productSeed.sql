abbf446ae8af18730ac4e16846bea6c4af699212
Adolfo <adolfo1997a@gmail.com>
2020-06-02 17:46:00 -0400

    Moving scripts for database folder

---
+++ docs/database/productSeed.sql
@@ -0,0 +1,8 @@
+insert into products(product_name, product_price, created_at, updated_at) values('napolitan pizza',10.3,now(), now());
+insert into products(product_name, product_price, created_at, updated_at) values('venezuelan pizza',10.1,now(), now());
+insert into products(product_name, product_price, created_at, updated_at) values('margaritan pizza',9.2,now(), now());
+insert into products(product_name, product_price, created_at, updated_at) values('half pizza',7.3,now(), now());
+insert into products(product_name, product_price, created_at, updated_at) values('mexican pizza',7.3,now(), now());
+insert into products(product_name, product_price, created_at, updated_at) values('ocean pizza',12.3, now(), now());
+insert into products(product_name, product_price, created_at, updated_at) values('vegetarian pizza',10.3,now(), now());
+insert into products(product_name, product_price, created_at, updated_at) values('american pizza',10.3,now(), now());
