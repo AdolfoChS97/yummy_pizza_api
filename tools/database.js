5e10c4df4b999daa3dddd96cefae5a7755dd4641
Adolfo <adolfo1997a@gmail.com>
2020-06-02 19:13:17 -0400

    Handling node_env

--- tools/database.js
+++ tools/database.js
@@ -1,12 +1,23 @@
 const { Client } = require("pg");
 require("dotenv").config();
 
-const client = new Client({
-  user: process.env.USER_DB,
-  host: process.env.HOST_DB,
-  database: process.env.NAME_DB,
-  password: process.env.PASSWORD_DB,
-  port: 5432,
-});
+if ((process.env.NODE_ENV || "").trim() !== "production") {
+  const client = new Client({
+    user: process.env.USER_DB,
+    host: process.env.HOST_DB,
+    database: process.env.NAME_DB,
+    password: process.env.PASSWORD_DB,
+    port: 5432,
+  });
+} else {
+  const client = new Client({
+    user: "hmqoueoyukzmbk",
+    host: "ec2-52-7-39-178.compute-1.amazonaws.com",
+    database: "d8aiqpt67ji0ji",
+    password:
+      "58c9bfd6598f1995759fd9cef4eba34f101215f31cd8af4236bc253034573dec",
+    port: 5432,
+  });
+}
 
 module.exports = client;
