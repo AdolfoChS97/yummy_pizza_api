d1168694cca5413e1f96ebbf79be1c1e918dee4f
Adolfo <adolfo1997a@gmail.com>
2020-06-08 15:34:54 -0400

    - Added the same middleware for all routes less the routes inside folder shared.

--- routes/products/index.js
+++ routes/products/index.js
@@ -2,6 +2,22 @@ const express = require("express");
 const ApiController = require("./../../controllers");
 const router = express.Router();
 
+router.use(async (req, res, next) => {
+    let d = new Date();
+    console.log(`Time:${d}`);
+    let result = await Jwt.validToken(req.headers.authorization.split(" ")[1]);
+    if (!result) {
+        res.send({
+            result: false,
+            data: null,
+            errors: {
+                message: 'token provided is not valid'
+            },
+        });
+    }
+    next();
+});
+
 router.get("/products", async (req, res) => {
     let response = await ApiController.getProducts();
     res.send(response);
